4d354daba7b5bd42d5261cc19f48217ab6b4a819
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2022-08-08 11:36:39 +0200

    Home styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
+import { Layout } from "antd";
+import styles from "./styles.js";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const { Footer } = Layout;
+
+const App = () => {
+
+  return (
+    <Router>
+      <Layout style={styles.layout}>
+        <div>Hello InstaBesties !! </div>
+        <Footer style={styles.footer}> InstaBesties 2022 !! </Footer>
+      </Layout>
+    </Router>
+  );
 
 
-const App = () => <div>Hello Bestie</div>;
+
+};
 
 export default App;
