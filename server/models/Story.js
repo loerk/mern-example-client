fb3d794df0ac18e703f0ec7b23aed35ba570e34a
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2022-08-08 08:35:12 +0200

    last update

--- server/models/Story.js
+++ server/models/Story.js
@@ -1,15 +1,19 @@
-/**
- * {
- * caption : String
- * username: String
- * userId: reference to userschema
- * image: String // (url/path)  of the image
- * tages : [String]
- * likes:{
- * type:[String]
- * default:[]
- * createdAt: new Date()
- * }
- * 
- * }
- */
+import mongoose from "mongoose";
+
+const storySchema = mongoose.Schema({
+  caption: { type: String, required: true },
+  username: { type: String, required: true },
+  userId: { type: String, required: true },
+  image: { type: String, required: true },
+  tags: String,
+  likes: {
+    type: [String],
+    default: [],
+  },
+  postDate: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+export default mongoose.model("Story", storySchema);
--- server/routes/storyRouter.js
+++ server/routes/storyRouter.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import { auth } from '../middlewares/verifyToken.js';
-
+import {
+  getStories,
+  createStory,
+  updateStory,
+  deleteStory,
+  likeStory,
+} from "../controllers/storyControllers.js";
 
 const router = Router();
 
 // get , add, edit , delete, likestory
-
+router.get("/getstories", getStories);
+router.post("/createstory", auth, createStory);
+router.put("/updatestory/:id", auth, updateStory);
+router.delete("/deletestory/:id", auth, deleteStory);
+router.put("/likestory/:id", auth, likeStory);
 
 export default router;
