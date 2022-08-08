fc822e374bf1b15ebc08264c20b9f59aaceb5fa6
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2022-08-08 12:07:24 +0200

    adding the Logo

---
+++ client/src/components/AppBar/AppBar.jsx
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from 'react';
+import { Layout, Image, Typography, Button, Avatar } from 'antd';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Logo from '../../resources/images/InstaBestiesLogo.png';
