de2610e1b16586eb3fc9cc2ad6c63089b8d5e9ae
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2022-08-09 11:12:10 +0200

    apis are added

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,1 +1,84 @@
 import axios from 'axios';
+
+// init axios instance
+
+/**
+ * Create new instance of axios with a custom config
+ */
+
+const api = axios.create({ baseURL: "http://localhost:8002" });
+
+
+/* 
+    * check if there is a token provided (localStorage ) before sending the request
+    * if yes then use that token with every request
+*/
+
+api.interceptors.request.use(
+    (req) => {
+        if (localStorage.getItem("auth")) {
+            req.headers.Authorization = `Bearer ${
+                JSON.parse(localStorage.getItem("auth")).token
+            }`;
+        }
+}
+);
+
+/**
+ * Auth requests
+ */
+/**
+ * 
+ * @param {formValues} user 
+ * @returns {Promise<JSON>}
+ */
+export const signup = (user) => api.post("/user/signup", user);
+
+/**
+ * 
+ * @param {formValues} user 
+ * @returns {Promise<JSON>}
+ */
+export const login = (user) => api.post("/user/signin", user);
+
+
+/**
+ * Post requests
+ */
+/**
+ * 
+ * @returns {Promise<JSON>}
+ */
+export const fetchPosts = () => api.get("/posts/getposts");
+
+/**
+ * 
+ * @param {formValues} newPost 
+ * @returns {Promise<JSON>}
+ */
+export const createPost = (newPost) => api.post("/posts/addpost", newPost);
+
+/**
+ * 
+ * @param {ObjectId} id 
+ * @param {formValues} updatedPost 
+ * @returns {Promise<JSON>}
+ */
+export const updatePost = (id, updatedPost) => api.put(`/posts/editpost/${id}`, updatedPost);
+
+/**
+ * 
+ * @param {ObjectId} id 
+ * @returns {Promise<JSON>}
+ */
+export const likePost = (id) => api.put(`/posts/likepost/${id}`);
+
+/**
+ * 
+ * @param {ObjectId} id 
+ * @returns {Promise<JSON>}
+ */
+export const deletePost = (id) => api.delete(`/posts/removepost/${id}`);
+
+
+
