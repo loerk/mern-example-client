48c4d5546a9d93ac47308725ccbbf08a2fd46153
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2022-08-10 12:03:45 +0200

    adding the Story component

--- client/src/components/Story/Story.jsx
+++ client/src/components/Story/Story.jsx
@@ -2,8 +2,89 @@ import { useState } from 'react';
 import { Card, Tooltip, Typography, Image } from 'antd';
 import { EditOutlined, DeleteTwoTone, HeartTwoTone } from '@ant-design/icons';
 import styles from "./styles.js";
+import moment from 'moment';
 
 
-const Story = ({setSelectedId}) => { };
+// import the context actions : delete or likeStory
+
+const { Meta } = Card;
+const { Link, Paragraph, Text } = Typography;
+
+const Story = ({ story, setSelectedId }) => { 
+    const [expand, setExpand] = useState(true);
+    const user = JSON.parse(localStorage.getItem('profile'));
+    const cardActions = [
+        <div style={styles.actions}>
+            <Tooltip
+                placement='top'
+                title="Like "
+                color="magenta"
+              //onClick : dipatch like a story
+                
+            >
+                <HeartTwoTone twoToneColor="magenta" />
+                &nbsp; {story.likes.length}
+            </Tooltip>
+        </div>,
+       <Tooltip
+                placement='top'
+                title="Edit" >
+           <EditOutlined
+               onClick={() => {
+                   setSelectedId(story._id);
+               }}
+            />
+        </Tooltip>,
+        <Tooltip
+                placement='top'
+                title="Delete"
+                color="red" >
+            <DeleteTwoTone
+                twoToneColor="red"
+               // select the id to delete with more attention
+            />
+        </Tooltip>
+    ]
+    return (
+        <Card
+            style={styles.card}
+            cover={<Image src={story.image} />}
+            actions={
+                user?.result?.id === story.userId
+                    ? cardActions
+                    : user?.result
+                        ? cardActions.slice(0, 1)
+                        : null
+            }
+        >
+            <Meta title={story.username} />
+            <Paragraph
+                style={{ margin: 0 }}
+                ellipsis={{
+                    rows: 2,
+                    expandable: true,
+                    symbol: "more",
+                    onExpand: () => {
+                        setExpand(true);
+                    },
+                    onEllipsis: () => {
+                        setExpand(false);
+                    }
+                }}
+            >
+                {story.caption}
+            </Paragraph>
+            {
+                expand ? (
+                    <Link href='#'>{story.tags.split(' ').map(tag => `#${tag}`)}</Link>
+                ) : null
+            };
+            <br />
+            <Text type='secondary'>
+                {moment(story.postDate).fromNow()}
+            </Text>
+        </Card>
+    );
+};
 
 export default Story;
